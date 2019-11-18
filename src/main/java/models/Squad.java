f586c5f625fe89228d29e4895fd817f4d93456a0
pkminor <minorpk@gmail.com>
2019-11-18 11:37:00 +0300

    updated Squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,92 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Squad {
+
+    private int id;
+    private String name;
+    private String cause;
+    private int max_size;
+    private Hero hero;
+    private static List<Squad> instances = new ArrayList<Squad>();
+
+    public Squad(String name, String cause, int max_size, Hero hero) {
+        this.name = name;
+        this.cause = cause;
+        this.max_size = max_size;
+        this.hero = hero;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getMax_size() {
+        return max_size;
+    }
+    public void setMax_size(int max_size) {
+        this.max_size = max_size;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+    public void setHero(Hero hero) {
+        this.hero = hero;
+    }
+
+    public void setNameCauseSizeHero(String name, String cause, int size, Hero hero){
+        this.setName(name);
+        this.setCause(cause);
+        this.setMax_size(size);
+        this.setHero(hero);
+    }
+
+    public static List<Squad> getInstances() {
+        return instances;
+    }
+    public static Squad findById(int id){ return instances.get(id-1); }
+
+    public static void clearAll() {
+        instances.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Squad squad = (Squad) o;
+        return id == squad.id &&
+                max_size == squad.max_size &&
+                Objects.equals(name, squad.name) &&
+                Objects.equals(cause, squad.cause) &&
+                Objects.equals(hero, squad.hero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, cause, max_size, hero);
+    }
 }
