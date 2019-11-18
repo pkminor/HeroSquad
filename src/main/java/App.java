9a4d07d3fc800e148e9a8ef248a1787edd5554e9
pkminor <minorpk@gmail.com>
2019-11-18 12:05:58 +0300

    added home route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
 
+        //GET : home:
+        get("/",(res,req)->home(), new HandlebarsTemplateEngine());
+    }
+
+    private static ModelAndView home(){
+        return new ModelAndView(new HashMap<String,Object>(), "index.hbs");
     }
 }
