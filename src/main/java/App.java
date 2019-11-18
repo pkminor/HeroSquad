a67f0b26a7fc9abc876ee99e3fd0cfbf7fa84411
pkminor <minorpk@gmail.com>
2019-11-18 16:39:14 +0300

    updated routes to include free heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static spark.Spark.*;
 
@@ -19,15 +20,29 @@ public class App {
         //GET : home:
         get("/",(req,res)->home(req), new HandlebarsTemplateEngine());
 
+        //POST : new hero
         post("hero/new", (req,res)->newHero(req), new HandlebarsTemplateEngine());
+
+        //POST : new squad
         post("squad/new", (req,res)->newSquad(req), new HandlebarsTemplateEngine());
     }
 
+
     private static ModelAndView home(Request request){
+
+        //List<Hero> all_heroes = request.session().attribute("heroes");
+        //List<Hero> engaged_heroes = Squad.getAllHeroes();
+       List<Hero> free_heroes = Squad.getFreeHeroes();
+       /*engaged_heroes.size() <1 ? all_heroes:
+                 all_heroes.size() > 0 ?
+                 all_heroes.stream()
+                .filter(hr-> engaged_heroes.contains(hr) == false)
+                .collect(Collectors.toList()) : new ArrayList<>();*/
 
         Map<String,Object> model = new HashMap<>();
         model.put("heroes", request.session().attribute("heroes"));
         model.put("squads", request.session().attribute("squads"));
+        model.put("free_heroes", free_heroes);
 
         return new ModelAndView(model, "index.hbs");
     }
@@ -42,9 +57,20 @@ public class App {
 
         request.session().attribute("heroes", Hero.getInstances());
 
+        //the dao/model should handle all data querying
+        //List<Hero> all_heroes = request.session().attribute("heroes");
+        //List<Hero> engaged_heroes = Squad.getAllHeroes();
+        List<Hero> free_heroes =Squad.getFreeHeroes();
+        /* engaged_heroes.size() <1 ? all_heroes:
+                all_heroes.size() > 0 ?
+                all_heroes.stream()
+                .filter(hr-> engaged_heroes.contains(hr) == false)
+                .collect(Collectors.toList()) : new ArrayList<>();*/
+
         Map<String, Object> model = new HashMap<>();
         model.put("heroes", request.session().attribute("heroes"));
         model.put("squads", request.session().attribute("squads"));
+        model.put("free_heroes", free_heroes);
 
         return new ModelAndView(model,"index.hbs");
     }
@@ -60,9 +86,19 @@ public class App {
 
         request.session().attribute("squads", Squad.getInstances());
 
+        List<Hero> all_heroes = request.session().attribute("heroes");
+        //List<Hero> engaged_heroes = Squad.getAllHeroes();
+        List<Hero> free_heroes = Squad.getFreeHeroes();
+        /* engaged_heroes.size() <1 ? all_heroes:
+                all_heroes.size() > 0 ?
+                all_heroes.stream()
+                .filter(hr-> engaged_heroes.contains(hr) == false)
+                .collect(Collectors.toList()) : new ArrayList<>();*/
+
         Map<String, Object> model = new HashMap<>();
         model.put("squads", request.session().attribute("squads"));
         model.put("heroes", request.session().attribute("heroes"));
+        model.put("free_heroes", free_heroes);
 
         return new ModelAndView(model,"index.hbs");
     }
