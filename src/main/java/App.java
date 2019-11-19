87cd9a5d79729d0ac5144d9cfc4eef3c97e65296
pkminor <minorpk@gmail.com>
2019-11-19 11:05:43 +0300

    prep for heroku deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,20 @@ public class App {
 
         staticFileLocation("/public");
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         //GET : home:
         get("/",(req,res)->home(req), new HandlebarsTemplateEngine());
         //POST : new hero
